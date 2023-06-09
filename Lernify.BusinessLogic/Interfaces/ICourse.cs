610f796621fbe1dffb89b401e53db62b7a783dc1
StefanVlasitchi <svlasitchi@gmail.com>
2023-06-09 15:07:12 +0300

    database for adding a course in chart

--- Lernify.BusinessLogic/Interfaces/ICourse.cs
+++ Lernify.BusinessLogic/Interfaces/ICourse.cs
@@ -1,12 +1,24 @@
-﻿using System;
+﻿using Lernify.Domain.Entities.Course;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
+using Lernify.Domain.Entities.User;
 
 namespace Lernify.BusinessLogic.Interfaces
 {
-     internal class ICourse
+     public interface ICourse
      {
+          List<Course> GetList();
+
+
+          Course GetbyId(int id);
+
+
+          ULoginResp Add(Course course);
+
+
+          ULoginResp Edit(Course course);
+
+
+          void Delete(int id);
      }
 }
