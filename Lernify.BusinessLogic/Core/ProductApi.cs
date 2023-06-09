f37c24a90605c06e5bccd6d76ed6e48e15333d3a
IonArsene <ion.arsene@isa.utm.md>
2023-06-09 14:15:47 +0300

    fixinng teacher in productapi

--- Lernify.BusinessLogic/Core/ProductApi.cs
+++ Lernify.BusinessLogic/Core/ProductApi.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Lernify.BusinessLogic.DBModel;
+using Lernify.Domain.Entities.Teacher;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -37,6 +39,70 @@ namespace Lernify.BusinessLogic.Core
                     var result = db.Reviews.ToList();
                     var reviewData = mapper.Map<List<Review>>(result);
                     return reviewData;
+               }
+          }
+
+
+          internal List<Teacher> GetTeacherList()
+          {
+               List<TDbTable> context;
+
+               var mapper = new MapperConfiguration(cfg => cfg.CreateMap<TDbTable, Teacher>()).CreateMapper();
+               using (var db = new TeacherContext())
+               {
+                    context = db.Teacher.ToList();
+               }
+               return mapper.Map<List<Teacher>>(context);
+          }
+
+          internal ULoginResp AddNewTeacher(Teacher data)
+          {
+               IMapper mapper = new MapperConfiguration(cfg => cfg.CreateMap<Teacher, TDbTable>()).CreateMapper();
+               var context = mapper.Map<TDbTable>(data);
+
+               using (var db = new TeacherContext())
+               {
+                    db.Teacher.Add(context);
+                    db.SaveChanges();
+               }
+               return new ULoginResp { Status = true };
+          }
+          internal Teacher GetTeacher(int id)
+          {
+               TDbTable context;
+               using (var db = new TeacherContext())
+                    context = db.Teacher.FirstOrDefault(u => u.Id == id);
+               IMapper mapper = new MapperConfiguration(cfg => cfg.CreateMap<TDbTable, Teacher>()).CreateMapper();
+
+               return context != null ? mapper.Map<Teacher>(context) : null;
+          }
+          internal ULoginResp UpdateTea(Teacher data)
+          {
+               if (GetTeacher(data.Id) == null)
+                    return new ULoginResp { Status = false, StatusMsg = "An Error occur at updating" };
+
+               IMapper mapper = new MapperConfiguration(cfg => cfg.CreateMap<Teacher, TDbTable>()).CreateMapper();
+               var result = mapper.Map<TDbTable>(data);
+
+               using (var db = new TeacherContext())
+               {
+                    db.Teacher.AddOrUpdate(result);
+                    db.SaveChanges();
+               }
+               return new ULoginResp { Status = true };
+
+          }
+
+          internal void DeleteProfessor(int id)
+          {
+               using (var db = new TeacherContext())
+               {
+                    var teacher = db.Teacher.FirstOrDefault(u => u.Id == id);
+                    if (teacher != null)
+                    {
+                         db.Teacher.Remove(teacher);
+                         db.SaveChanges();
+                    }
                }
           }
      }
