efe891299625b3af88e0b703f12b72a5eb3a5eda
victoruntila <untilavictor02@gmail.com>
2023-06-09 12:08:45 +0300

    Session and Minimla

--- Lernify.Domain/Entities/User/Session.cs
+++ Lernify.Domain/Entities/User/Session.cs
@@ -1,12 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Lernify.Domain.Entities.User
 {
-     internal class Class1
+
+     public class Session
      {
+          [Key]
+          [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+          public int SessionId { get; set; }
+
+          [Required]
+          [StringLength(30)]
+          public string Username { get; set; }
+
+          [Required]
+          public string CookieString { get; set; }
+
+          [Required]
+          public DateTime ExpireTime { get; set; }
      }
 }
